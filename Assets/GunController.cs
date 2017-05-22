6f1d7f66ab27f8c0b48e49f54ea0d54d5ae4d81c
FukuokaMao <mao_1237@yahoo.co.jp>
2017-05-23 08:50:23 +0900

    Change of variable name,Unification of naming convention

--- Assets/GunController.cs
+++ Assets/GunController.cs
@@ -5,43 +5,43 @@ using UnityEngine.UI;
 
 public class GunController : MonoBehaviour {
 
-	public GameObject Sparkle;
+	public GameObject sparkle;
 	public AudioClip shotgun;
 	public AudioClip reload;
 	AudioSource audioSource;
-	public Text[] text = new Text[2];
-	int Bullet = 30;
-	int BulletBox = 150;
+	public Text[] shotbullet = new Text[2];
+	int bullet = 30;
+	int bulletbox = 150;
 
 	void Start (){
 		audioSource = GetComponent<AudioSource>();
-		text [0].text = "BulletBox : " + BulletBox;
-		text [1].text = "Bullet : " + Bullet + "/30";
+		shotbullet [0].text = "BulletBox : " + bulletbox;
+		shotbullet [1].text = "Bullet : " + bullet + "/30";
 	}
 
 	// 銃口に火をつける
 	void Fire(){
-		GameObject fire = Instantiate (Sparkle,transform.position,Quaternion.identity);
+		GameObject fire = Instantiate (sparkle,transform.position,Quaternion.identity);
 		Destroy (fire,0.2f);
 	}
 		
 	//弾数を減らす
 	void Shot(){
-		Bullet -= 1;
-		text [1].text = "Bullet : " + Bullet + "/30";
+		bullet -= 1;
+		shotbullet [1].text = "Bullet : " + bullet + "/30";
 	}
 
 	//リロード機能
 	void Reload(){
-		BulletBox = BulletBox - (30 - Bullet);
-		Bullet = (30 - Bullet) + Bullet;
-		text [0].text = "BulletBox : " + BulletBox;
-		text [1].text = "Bullet : " + Bullet + "/30";
+		bulletbox = bulletbox - (30 - bullet);
+		bullet = (30 - bullet) + bullet;
+		shotbullet [0].text = "BulletBox : " + bulletbox;
+		shotbullet [1].text = "Bullet : " + bullet + "/30";
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0) && Bullet > 0 )
+		if(Input.GetMouseButtonDown(0) && bullet > 0 )
 		{	
 			// 銃口に火をつける
 			Fire();
@@ -58,7 +58,7 @@ public class GunController : MonoBehaviour {
 			if (Physics.Raycast (ray, out hit)) 
 			{
 				//銃弾が当たった所で火をつける
-				GameObject bullet = Instantiate(Sparkle,hit.point,Quaternion.identity);
+				GameObject bullet = Instantiate(sparkle,hit.point,Quaternion.identity);
 				Destroy(bullet,0.2f);
 			}
 		}
