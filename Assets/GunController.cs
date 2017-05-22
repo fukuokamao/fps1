e92cfc093b922f562a0010a6bfb9bd325e840e9c
FukuokaMao <mao_1237@yahoo.co.jp>
2017-05-23 08:15:48 +0900

    Implementation of reload function

--- Assets/GunController.cs
+++ Assets/GunController.cs
@@ -1,33 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunController : MonoBehaviour {
 
 	public GameObject Sparkle;
-	public AudioClip Shotgun;
+	public AudioClip shotgun;
+	public AudioClip reload;
 	AudioSource audioSource;
+	public Text[] text = new Text[2];
+	int Bullet = 30;
+	int BulletBox = 150;
 
 	void Start (){
 		audioSource = GetComponent<AudioSource>();
+		text [0].text = "BulletBox : " + BulletBox;
+		text [1].text = "Bullet : " + Bullet + "/30";
 	}
 
 	// 銃口に火をつける
 	void Fire(){
-		GameObject fire =
-		Instantiate (Sparkle,transform.position,Quaternion.identity);
+		GameObject fire = Instantiate (Sparkle,transform.position,Quaternion.identity);
 		Destroy (fire,0.2f);
 	}
+		
+	//弾数を減らす
+	void Shot(){
+		Bullet -= 1;
+		text [1].text = "Bullet : " + Bullet + "/30";
+	}
 
+	//リロード機能
+	void Reload(){
+		BulletBox = BulletBox - (30 - Bullet);
+		Bullet = (30 - Bullet) + Bullet;
+		text [0].text = "BulletBox : " + BulletBox;
+		text [1].text = "Bullet : " + Bullet + "/30";
+	}
 
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown(0))
+		if(Input.GetMouseButtonDown(0) && Bullet > 0 )
 		{	
 			// 銃口に火をつける
 			Fire();
+
+			//弾数を減らす
+			Shot();
+
 			// 発射音を鳴らす
-			audioSource.PlayOneShot(Shotgun,0.5f);
+			audioSource.PlayOneShot(shotgun,0.5f);
 
 			Ray ray = new Ray(transform.position, transform.forward);
 			RaycastHit hit;
@@ -38,6 +61,15 @@ public class GunController : MonoBehaviour {
 				GameObject bullet = Instantiate(Sparkle,hit.point,Quaternion.identity);
 				Destroy(bullet,0.2f);
 			}
+		}
+
+		if (Input.GetKey (KeyCode.R)) 
+		{
+			//リロード機能
+			Reload();
+
+			//リロード音を鳴らす
+			audioSource.PlayOneShot(reload,0.5f);
 		}
 	}
 }
